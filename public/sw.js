1766e8bcbc0510e4fb01321f3f44973e30a39030
loginVeb <loginVeb@yandex.ru>
2025-07-10 16:31:00 +0300

    modified:   public/sw.js В проект внедрена стратегия кэширования Stale While Revalidate в сервис-воркере для оптимизации работы с кэшем

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,29 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-  self.skipWaiting();
+const CACHE_NAME = 'cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Добавьте сюда другие ресурсы, которые хотите кэшировать
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating.');
-});
-
-self.addEventListener('fetch', (event) => {
-  // You can add custom fetch handling here if needed
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      const fetchPromise = fetch(event.request).then(networkResponse => {
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
+    })
+  );
 });
